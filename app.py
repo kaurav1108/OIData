10b8d207156f599a8246e6e63a687e6e114449a3
kaurav1108 <71206196+kaurav1108@users.noreply.github.com>
2023-03-13 18:11:22 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,199 @@
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+niftyunderlyingsql="""
+select underlying,expiryDate,underlyingValue  FROM fnodatabase.niftyoidata WHERE expiryDate = '16-Mar-2023' LIMIT 1;
+"""
+bniftyunderlyingsql="""
+select underlying,expiryDate,underlyingValue  FROM fnodatabase.bankniftyoidata WHERE expiryDate = '16-Mar-2023' LIMIT 1;
+"""
+niftyoisql="""
+Select 
+	CASE
+	WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) < 0 THEN 'Long Liquidation'
+    WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) > 0 THEN 'Short Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) > 0 THEN 'Long Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) < 0 THEN 'Short covering'
+    END AS 'CE_Signal',
+  MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END) AS 'CE_OpenInterest',
+  MAX(CASE WHEN instrumentType = 'CE' THEN changeinOpenInterest END) AS 'CE_ChangeinOpenInterest',
+  MAX(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END) AS 'CE_TotalTradedVolume',
+  MAX(CASE WHEN instrumentType = 'CE' THEN impliedVolatility END) AS 'CE_ImpliedVolatility',
+  MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END) AS 'CE_Change',
+  strikePrice,
+  MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END) AS 'PE_Change',
+  MAX(CASE WHEN instrumentType = 'PE' THEN impliedVolatility END) AS 'PE_ImpliedVolatility',
+  MAX(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END) AS 'PE_TotalTradedVolume',
+  MAX(CASE WHEN instrumentType = 'PE' THEN changeinOpenInterest END) AS 'PE_ChangeinOpenInterest',
+  MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END) AS 'PE_OpenInterest',
+  CASE
+	WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) < 0 THEN 'Long Liquidation'
+    WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) > 0 THEN 'Short Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) > 0 THEN 'Long Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) < 0 THEN 'Short covering'
+    END AS 'PE_Signal'
+FROM fnodatabase.niftyoidata
+WHERE expiryDate = '16-Mar-2023' AND (instrumentType = 'CE' OR instrumentType = 'PE')
+GROUP BY strikePrice
+ORDER BY strikePrice;
+"""
+bniftyoisql="""
+Select 
+	CASE
+	WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) < 0 THEN 'Long Liquidation'
+    WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) > 0 THEN 'Short Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) > 0 THEN 'Long Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END)) < 0 THEN 'Short covering'
+    END AS 'CE_Signal',
+  MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END) AS 'CE_OpenInterest',
+  MAX(CASE WHEN instrumentType = 'CE' THEN changeinOpenInterest END) AS 'CE_ChangeinOpenInterest',
+  MAX(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END) AS 'CE_TotalTradedVolume',
+  MAX(CASE WHEN instrumentType = 'CE' THEN impliedVolatility END) AS 'CE_ImpliedVolatility',
+  MAX(CASE WHEN instrumentType = 'CE' THEN round(`change`,3) END) AS 'CE_Change',
+  strikePrice,
+  MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END) AS 'PE_Change',
+  MAX(CASE WHEN instrumentType = 'PE' THEN impliedVolatility END) AS 'PE_ImpliedVolatility',
+  MAX(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END) AS 'PE_TotalTradedVolume',
+  MAX(CASE WHEN instrumentType = 'PE' THEN changeinOpenInterest END) AS 'PE_ChangeinOpenInterest',
+  MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END) AS 'PE_OpenInterest',
+  CASE
+	WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) < 0 THEN 'Long Liquidation'
+    WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) < 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) > 0 THEN 'Short Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) > 0 THEN 'Long Buildup'
+    WHEN (MAX(CASE WHEN instrumentType = 'PE' THEN round(`change`,3) END)) > 0 AND (MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END)) < 0 THEN 'Short covering'
+    END AS 'PE_Signal'
+FROM fnodatabase.bankniftyoidata
+WHERE expiryDate = '16-Mar-2023' AND (instrumentType = 'CE' OR instrumentType = 'PE')
+GROUP BY strikePrice
+ORDER BY strikePrice;
+"""
+breakniftyoisql="""
+select 
+round(MAX(CASE WHEN instrumentType = 'CE' THEN impliedVolatility END) - MAX(CASE WHEN instrumentType = 'PE' THEN impliedVolatility END),2) AS 'BreakIV',
+MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END) - MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END) AS 'BreakOI',
+MAX(CASE WHEN instrumentType = 'CE' THEN changeinOpenInterest END) - MAX(CASE WHEN instrumentType = 'PE' THEN changeinOpenInterest END) AS 'BreakCOI',
+strikePrice,
+  CASE
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 4)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 3)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0'
+  END AS 'CNT',
+    ROUND(MAX(CASE WHEN instrumentType = 'CE' THEN changeinOpenInterest END) / 
+   CASE
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 4)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 3)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0' END) AS 'CCOIBYNT',
+  CASE
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 4)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 3)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0'
+   END AS 'PNT',
+    ROUND(MAX(CASE WHEN instrumentType = 'PE' THEN changeinOpenInterest END) / 
+   CASE
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 4)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 3)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0' END) AS 'PCOIBYNT'
+   FROM fnodatabase.niftyoidata
+WHERE expiryDate = '16-Mar-2023' AND (instrumentType = 'CE' OR instrumentType = 'PE')
+GROUP BY strikePrice
+ORDER BY strikePrice;
+"""
+breakbniftyoisql="""
+select 
+round(MAX(CASE WHEN instrumentType = 'CE' THEN impliedVolatility END) - MAX(CASE WHEN instrumentType = 'PE' THEN impliedVolatility END),2) AS 'BreakIV',
+MAX(CASE WHEN instrumentType = 'CE' THEN openInterest END) - MAX(CASE WHEN instrumentType = 'PE' THEN openInterest END) AS 'BreakOI',
+MAX(CASE WHEN instrumentType = 'CE' THEN changeinOpenInterest END) - MAX(CASE WHEN instrumentType = 'PE' THEN changeinOpenInterest END) AS 'BreakCOI',
+strikePrice,
+  CASE
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 4)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 3)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0'
+  END AS 'CNT',
+    ROUND(MAX(CASE WHEN instrumentType = 'CE' THEN changeinOpenInterest END) / 
+   CASE
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 4)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 3)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'CE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0' END) AS 'CCOIBYNT',
+  CASE
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 4)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 3)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0'
+   END AS 'PNT',
+    ROUND(MAX(CASE WHEN instrumentType = 'PE' THEN changeinOpenInterest END) / 
+   CASE
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 9 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 4)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 8 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 3)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 7 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 2)))
+	WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 6 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    WHEN MAX(LENGTH(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END)) = 5 THEN (MAX(LEFT(CASE WHEN instrumentType = 'PE' THEN (totalTradedVolume*50) END, 1)))
+    ELSE '0' END) AS 'PCOIBYNT'
+   FROM fnodatabase.bankniftyoidata
+WHERE expiryDate = '16-Mar-2023' AND (instrumentType = 'CE' OR instrumentType = 'PE')
+GROUP BY strikePrice
+ORDER BY strikePrice;
+"""
+
+# configure MySQL connection
+cnx = mysql.connector.connect(user='test', password='2035',
+                              host='localhost', port= 3306, database='fnodatabase')
+cursor = cnx.cursor()
+
+@app.route('/')
+def index():
+    # execute first query and fetch results
+    return render_template('indexold.html')
+
+@app.route("/page1")
+def page1():
+
+    # execute niftyunderlying query and fetch results
+    cursor.execute(niftyunderlyingsql)
+    niftyunderlying = cursor.fetchall()
+
+    # execute bniftyunderlying query and fetch results
+    cursor.execute(bniftyunderlyingsql)
+    bniftyunderlying = cursor.fetchall()
+
+    # execute niftyoi query and fetch results
+    cursor.execute(niftyoisql)
+    niftyoi = cursor.fetchall()
+
+    # execute bniftyoi query and fetch results
+    cursor.execute(bniftyoisql)
+    bniftyoi = cursor.fetchall()
+
+    # execute breakniftyoi query and fetch results
+    cursor.execute(breakniftyoisql)
+    breakniftyoi = cursor.fetchall()
+
+    # execute breakbniftyoi query and fetch results
+    cursor.execute(breakbniftyoisql)
+    breakbniftyoi = cursor.fetchall()
+    
+    return render_template('index.html', niftyunderlying=niftyunderlying, bniftyunderlying=bniftyunderlying, niftyoi=niftyoi, bniftyoi=bniftyoi, breakniftyoi=breakniftyoi, breakbniftyoi=breakbniftyoi)
+
+if __name__ == '__main__':
+    app.run()
